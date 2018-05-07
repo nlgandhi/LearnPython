e111d81d40ff54897bca0de2c34934fbe3f24a24
Nitin Gandhi <nitin.gandhi@outlook.com>
2018-05-06 23:45:26 -0700

    dev

--- db1.py
+++ db1.py
@@ -1,13 +1,24 @@
 import pyodbc 
 
-cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=server_name;"
-                      "Database=db_name;"
-                      "Trusted_Connection=yes;")
+
+def QuantScreenRouter():
+    cnxn = pyodbc.connect(
+        r'Driver={SQL Server Native Client 11.0};'
+        r'SERVER=f6iq6q5hoj.database.windows.net;'
+        r'DATABASE=QuantValue;'
+        r'UID=connectsoft@f6iq6q5hoj;'
+        r'PWD=buysell1!'
+        )
 
 
-cursor = cnxn.cursor()
-cursor.execute('SELECT * FROM Table')
+    cursor = cnxn.cursor()
+    cursor.execute('Select top 10 * from [dbo].[TickerInfo]')
 
-for row in cursor:
-    print('row = %r' % (row,))
+    for row in cursor:
+        print(row.Symol)
+
+    cnxn.close()
+    cursor.close()
+
+
+QuantScreenRouter()
