63b2203841ef3439352fdf283c8ca2e21ddb2e83
Nitin Gandhi <nitin.gandhi@outlook.com>
2018-06-12 23:53:50 -0700

    Updates

--- api1.py
+++ api1.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import logging
 
 #Jasonify -  Flask offers the convenient jsonify() function, which returns a JSON object from Python variables:
 
@@ -22,6 +23,38 @@ def json_hello():
 def add_income():
   incomes.append(request.get_json())
   return '', 204
+
+@app.route('/LongAO')
+def QuantScreenRouter():
+
+    try:
+
+      logging.warning("before_request")
+
+      cnxn = pyodbc.connect(
+              r'Driver={SQL Server Native Client 11.0};'
+              r'SERVER=f6iq6q5hoj.database.windows.net;'
+              r'DATABASE=QuantValue;'
+              r'UID=connectsoft@f6iq6q5hoj;'
+              r'PWD=!'
+          )
+
+      cursor = cnxn.cursor()
+      cursor.execute("exec [dbo].[QuantScreenRouter] 'LONG_AO','Toronto'")
+      logging.info("has data")	
+      results = cursor.fetchall()
+      entries = [dict(title=row['title'], text=row[1]) for row in results]
+
+      #for row in cursor:
+      #    print(row.Symbol)
+
+      cnxn.close(cursor)
+      cursor.close()
+
+      return entries
+
+    except ValueError:
+        pass
 
 
 if __name__ == '__main__':
--- db1.py
+++ db1.py
@@ -1,23 +1,34 @@
 import pyodbc 
+import logging
 
 #Test This one first.
 def QuantScreenRouter():
-    cnxn = pyodbc.connect(
-        r'Driver={SQL Server Native Client 11.0};'
-        r'SERVER=f6iq6q5hoj.database.windows.net;'
-        r'DATABASE=QuantValue;'
-        r'UID=connectsoft@f6iq6q5hoj;'
-        r'PWD=buysell1!'
-        )
 
-    cursor = cnxn.cursor()
-    cursor.execute("exec [dbo].[QuantScreenRouter]('LONG_AO','Toronto')")
+    try:
+        cnxn = pyodbc.connect(
+                r'Driver={SQL Server Native Client 11.0};'
+                r'SERVER=f6iq6q5hoj.database.windows.net;'
+                r'DATABASE=QuantValue;'
+                r'UID=connectsoft@f6iq6q5hoj;'
+                r'PWD=!'
+            )
 
-    for row in cursor:
-        print(row.Symbol)
+        logging.warning("before_request")
+        cursor = cnxn.cursor()
+        cursor.execute("exec [dbo].[QuantScreenRouter] 'LONG_AO','Toronto'")
+        results = cursor.fetchall()
+        #entries = [dict(title=row['Symbol'], text=row[1]) for row in results]
 
-    cnxn.close()
-    cursor.close()
+        #for row in results:
+        #    print(row.Symbol)
+
+        cnxn.close()
+        cursor.close()
+        return results
+
+
+    except ValueError:
+        pass
 
 
 QuantScreenRouter()
