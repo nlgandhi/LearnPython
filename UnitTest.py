8e7ea5326ea2614d46645e49d130cf89ac3a9118
nitin.gandhi@outlook.com <nitin.gandhi@outlook.com>
2018-06-12 17:41:06 -0700

    Unit TESTS

---
+++ UnitTest.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+
+#def fun(x):
+#    return x + 1
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(fun(3), 4)
+
+
+if __name__ == ' __main__':
+    unittest.main()
+
+
+
+
