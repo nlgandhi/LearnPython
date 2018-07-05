3cac4a6b9485ef45ea79e39d1649006d5921ef84
Nitin Gandhi <nitin.gandhi@outlook.com>
2018-07-05 00:14:36 -0700

    Pandas

---
+++ pandas1.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import random
+
+
+# yield is a keyword that is used like return, except the function will return a generator.
+def lottery():
+    # returns 6 numbers between 1 and 40
+    for i in range(6):
+        yield random.randint(1, 40)
+
+    # returns a 7th number between 1 and 15
+    yield random.randint(1,15)
+
+# for random_number in lottery():
+#        print("Number is... %d!" %(random_number))
+
+
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+
+
+brics = pd.DataFrame(dict)
+
+# Pandas can be loaded directly from csv files
+# cars = pd.read_csv('cars.csv')
+# cars = pd.read_csv('cars.csv', index_col = 0)
+
+#Add new index values
+brics.index = ["BR", "RU", "IN", "CH", "SA"]
+
+# Is label-based, which means that you have to specify rows and columns based on their row and column label
+print(brics.loc[['IN', 'CH']])
+
+
+# print(brics)
