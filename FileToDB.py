a1613f71e143d46e88e380c3410a70a72c48c45a
Nitin Gandhi <nitin.gandhi@outlook.com>
2018-07-09 00:01:37 -0700

    done

--- FileToDB.py
+++ FileToDB.py
@@ -13,55 +13,70 @@ def ReadFile(filename):
     # a file
     # C:/Nitin/Data/FundamentalGrowthUS.csv
     # C:/Projects5/Pythion/TestProj1/Data/FundamentalGrowthUS.csv
-    myfile = open('C:/Nitin/Data/FundamentalGrowthUS.csv', mode='r', encoding='utf-8')
+    #myfile = open('C:/Nitin/Data/FundamentalGrowthUS.csv', mode='r', encoding='utf-8')
+    myfile = open('C:/Nitin/Data/FundamentalGrowthGlobal.csv', mode='r', encoding='utf-8')
+    
     #mytxt = myfile.read()
 
     for line in myfile:
         print(line)
     myfile.close()
 
-def ReadFilePandas():
+def ReadGrowthFile():
+    # try:
+
     colnames = ['Industry Name','Number of Firms','ROE','Retention Ratio','Fundamental Growth ']
     # C:/Nitin/Data/FundamentalGrowthUS.csv
-    # C:/Projects5/Pythion/TestProj1/Data/FundamentalGrowthUS.csv
-    data = pd.read_csv('C:/Nitin/Data/FundamentalGrowthUS.csv', usecols=colnames)     #,index_col = 'Industry Name'
+    # C:/Projects5/Pythion/TestProj1/Data/FundamentalGrowthUS.csv       
+    # data = pd.read_csv('C:/Projects5/Pythion/TestProj1/Data/FundamentalGrowthEmrg.csv', usecols=colnames)     #,index_col = 'Industry Name'
+    data = pd.read_csv('C:/Projects5/Pythion/TestProj1/Data/HIST/fundgr00.csv', usecols=colnames)     #,index_col = 'Industry Name'
+
     data['Country'] = 'USA'
-    print(data['Number of Firms']) 
-
-    for index, row in data.iterrows():
-        print(row['Industry Name'])
-        print(row['Number of Firms'])
-        print(row['ROE'])
-        print(row['Retention Ratio'])
-        print(row['Fundamental Growth '])
-
-    #data['Number of Firms'].plot()
+    data['Year'] = '2000'  
+    data = data.fillna(-99)     
 
     #print(data.shape)
 
-    #cnxn = pyodbc.connect(
-    #            r'Driver={SQL Server Native Client 11.0};'
-    #            r'SERVER=f6iq6q5hoj.database.windows.net;'
-    #            r'DATABASE=QuantValue;'
-    #            r'UID=connectsoft@f6iq6q5hoj;'
-    #            r'PWD=!'
-    #        )
+    cnxn = pyodbc.connect(
+                r'Driver={SQL Server Native Client 11.0};'
+                r'SERVER=f6iq6q5hoj.database.windows.net;'
+                r'DATABASE=QuantValue;'
+                r'UID=connectsoft@f6iq6q5hoj;'
+                r'PWD='
+            )
 
     #logging.warning("before_request")
-    #cursor = cnxn.cursor()
-    #cursor.execute("exec [dbo].[QSTickerSignal] 'NYSE'")
-    #cursor.execute("exec [dbo].[QSTickerSignal] 'NYSE'")
+    cursor = cnxn.cursor()    
 
-    #results = cursor.fetchall()
+    for index, row in data.iterrows():
+        if ( len(str(row['ROE'])) > 0): 
+            row['ROE'] = str(row['ROE']).replace("%", "")
+            row['ROE'] = str(row['ROE']).replace("NA", "")
+        if ( len(str(row['Retention Ratio'])) > 0):
+            row['Retention Ratio'] = str(row['Retention Ratio']).replace("%", "")     
+            row['Retention Ratio'] = str(row['Retention Ratio']).replace("NA", "")     
+        if ( len(str(row['Fundamental Growth '])) > 0):
+            row['Fundamental Growth '] = str(row['Fundamental Growth ']).replace("%", "")
+            row['Fundamental Growth '] = str(row['Fundamental Growth ']).replace("NA", "")
 
-    #for row in results:
-    #    print(row.Ticker)
+        params = (row['Country'],row['Year'],row['Industry Name'],row['Number of Firms'],
+                row['ROE'],row['Retention Ratio'],row['Fundamental Growth '])
+        cursor.execute('{CALL [NYUDataGrowth_Add](?,?,?,?,?,?,?)}', params)
+        cursor.commit()            
 
-    #cnxn.close()
-    #cursor.close()
+    cnxn.close()
+    cursor.close()
+
+    # except:
+    #     print("Error: ") #+ e.message
+
+    # finally:
+    #     print("Finally is called directly after executing the try statement whether an exception is thrown or not.")
+    #     cnxn.close()
+    #     cursor.close()
 
 
-ReadFilePandas()
+ReadGrowthFile()
 
 # main("Test")
 
