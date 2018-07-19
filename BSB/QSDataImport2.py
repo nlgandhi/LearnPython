186fdc9921cef9a263e5946646f378819841e593
Nitin Gandhi <nitin.gandhi@outlook.com>
2018-07-19 00:00:05 -0700

    done

--- BSB/QSDataImport2.py
+++ BSB/QSDataImport2.py
@@ -22,9 +22,38 @@ def ReadNYUCompanyLookup():
         #print(os.path.normpath(data_file_path))
 
         colnames = ['Industry Group','Company Name','Exchange:Ticker','Country','Broad Group','Sub Group']
-        data = pd.read_csv('C:/Projects1/QuantValuationData/Adamodar_NYU/2018/CompanyLookup/indname.csv', usecols=colnames,  encoding='utf-8') #index='PassengerId' 
+        data = pd.read_csv('C:/Projects1/QuantValuationData/Adamodar_NYU/2018/CompanyLookup/indname.csv', usecols=colnames,  encoding='ISO-8859-1') #index='PassengerId' 
+        data['Year'] = '2017'  
+        data['Ticker'] = ''
+        data['Exchange'] = '' 
         data.info()
 
+        #data = data.fillna(-99)     
+        #print(data.shape)
+
+        cnxn = pyodbc.connect(
+                    r'Driver={SQL Server Native Client 11.0};'
+                    r'SERVER=f6iq6q5hoj.database.windows.net;'
+                    r'DATABASE=QuantValue;'
+                    r'UID=@f6iq6q5hoj;'
+                    r'PWD=!'
+                )
+
+        #logging.warning("before_request")
+        cursor = cnxn.cursor()    
+
+        for index, row in data.iterrows():
+            x,y = row['Exchange:Ticker'].split(':')            
+            data['Ticker'] = y
+            data['Exchange'] = x 
+            
+        # params = (row['Country'],row['Year'],row['Industry Name'],row['Number of Firms'],
+        #        row['ROE'],row['Retention Ratio'],row['Fundamental Growth '])
+        # cursor.execute('{CALL [NYUDataGrowth_Add](?,?,?,?,?,?,?)}', params)
+        # cursor.commit()            
+
+        cnxn.close()
+        cursor.close()
 
 
     except:
